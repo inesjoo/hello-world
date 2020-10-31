6f27d487689599832981d10c19824d25e54112f2
inesjoo <73700665+inesjoo@users.noreply.github.com>
2020-10-31 21:19:18 +0900

    Add files via upload

---
+++ 14강_else.py
@@ -0,0 +1,128 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:23:07) [MSC v.1927 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=5
+>>> if x==10:
+	print('10입니다')
+else:
+	print('10이 아닙니ㅏㄷ.')
+
+	
+10이 아닙니ㅏㄷ.
+>>> if x==10:
+	print(' 10입니다')
+else:
+	print('xdp emfdjdlTsms tntwksms')
+	print('10이 아닙ㄴ디ㅏ.')
+
+	
+xdp emfdjdlTsms tntwksms
+10이 아닙ㄴ디ㅏ.
+>>> 
+>>> 
+>>> 
+>>> if x==10:
+	print('10입니다')
+else:
+	print('x에 들어있는 숫자는')
+print(
+	
+SyntaxError: invalid syntax
+>>> 
+>>> if x==10:
+	print('10입니다')
+else:
+	print('x에 들어있는 숫자는')
+print('10이 아닙니다')
+SyntaxError: invalid syntax
+>>> 들여쓰기 잘해라
+SyntaxError: invalid syntax
+>>> 
+>>> 
+>>> 
+>>> 
+>>> if True:
+	print('참')
+else:
+	print('거짓')
+
+	
+참
+>>> if False:
+	print('참')
+else:
+	print('거짓')
+
+	
+거짓
+>>> if None:
+	print('참')
+else:
+	print('거짓')
+
+	
+거짓
+>>> #True는 if의 코드가 실행된다
+>>> #False는 else의 코드가 실행된다.
+>>> #None은 False로 취급되므로 else의 코드가 실행된다
+>>> #숫자는 정수, 실수, 몇진법 상관없이 0이면 거짓, 0이 아닌 수는 참임
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> if 0:
+	print('charm')
+
+	
+>>> if 0:
+	print('cka')
+else:
+	print('rjwlt')
+
+	
+rjwlt
+>>> #문자열은 내용이 있으면 참, 비어있으면 거짓임.
+>>> 
+>>> if 'Hello':
+	print('cka')
+else:
+	print('rjwlt')
+
+	
+cka
+>>> if '':
+	print('cka)'
+else:
+	      
+SyntaxError: invalid syntax
+>>> if '':
+	print('cka')
+else:
+	print('rjwlt')
+
+	
+rjwlt
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> #조건식을 여러개 지정하기
+>>> 
+>>> x=10
+>>> y=20
+>>> if x==10 and y==20:
+	print('cka')
+else:
+	print('rjwlt')
+
+	
+cka
+>>> if 0<x<20:
+	print('20보다 작은 양수입니다')
+
+	
+20보다 작은 양수입니다
+>>> 
