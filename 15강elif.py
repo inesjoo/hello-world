f5f9bc02e837544135926bf80e8e5bdb35774d1f
inesjoo <73700665+inesjoo@users.noreply.github.com>
2020-10-31 21:38:18 +0900

    Add files via upload

---
+++ 15강elif.py
@@ -0,0 +1,44 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:23:07) [MSC v.1927 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=20
+>>> if x==10:
+	print('10입니다')
+elif x==20:
+	print('20입니다')
+
+	
+20입니다
+>>> ㅌ=30
+>>> del ㅌ
+>>> x=30
+>>> if x==10:
+	print('10입니다')
+elif x==20:
+	print('20입니다')
+else:
+	print('10도 20도 아닙니다.')
+
+	
+10도 20도 아닙니다.
+>>> 
+>>> 
+>>> 
+>>> 
+>>> #else는 맨 뒤에나와야지 elif앞에 나오면 문법적 오류임
+>>> 
+================ RESTART: C:/Users/TETRA/Desktop/elif_machine.py ===============
+1
+cola
+>>> 
+================ RESTART: C:/Users/TETRA/Desktop/elif_machine.py ===============
+2
+cider
+>>> 
+================ RESTART: C:/Users/TETRA/Desktop/elif_machine.py ===============
+3
+fanta
+>>> 
+================ RESTART: C:/Users/TETRA/Desktop/elif_machine.py ===============
+4
+beverage not offered
+>>> 
---
+++ elif_machine.py
@@ -0,0 +1,10 @@
+button=int(input())
+if button==1:
+    print('cola')
+elif button==2:
+    print('cider')
+elif button==3:
+    print('fanta')
+else:
+    print('beverage not offered')
+    
