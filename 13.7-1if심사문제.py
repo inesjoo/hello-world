e76e047a741326bf1dc933a21c2dbca33ec3db14
inesjoo <73700665+inesjoo@users.noreply.github.com>
2020-10-31 20:35:58 +0900

    Add files via upload

---
+++ 13.7-1if심사문제.py
@@ -0,0 +1,8 @@
+price=int(input())
+coupon=input()
+if coupon=='Cash3000':
+    price-=3000
+    print(price)
+if coupon=='Cash5000':
+    price-=5000
+    print(price)
---
+++ 13.7if심사문제.py
@@ -0,0 +1,6 @@
+price=int(input())
+coupon=str(input())
+if coupon=='Cash5000':
+    print(price-5000)
+if coupon=='Cash3000':
+    print(price-3000)
