48300e4808e24fdf91c4a38b887de310fbf60322
inesjoo <73700665+inesjoo@users.noreply.github.com>
2020-11-16 21:32:33 +0900

    Add files via upload

--- 19강_반복.py
+++ 19강_반복.py
@@ -1,5 +1,20 @@
-for i in range(5):
-    for j in range(7):
-        print('주영욱', end='')
-    print()
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:23:07) [MSC v.1927 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> for i in range(5):
+	for j in range (5):
+		print('j:',j, sep='',end=' ')
+	print('i:',i,'\\n',sep'')
+	
+SyntaxError: invalid syntax
+>>> for i in range(5):
+	for j in range (5):
+		print('j:',j, sep='',end=' ')
+	print('i:',i,'\\n',sep='')
 
+	
+j:0 j:1 j:2 j:3 j:4 i:0\n
+j:0 j:1 j:2 j:3 j:4 i:1\n
+j:0 j:1 j:2 j:3 j:4 i:2\n
+j:0 j:1 j:2 j:3 j:4 i:3\n
+j:0 j:1 j:2 j:3 j:4 i:4\n
+>>> 
---
+++ 19강_심사문제.py
@@ -0,0 +1,9 @@
+a=int(input())
+for i in range(a):
+    for j in range(a+i):
+        if j<=i:
+            print(' ',end='')
+        elif 
+        else:
+            print('*',end='')
+    print()
