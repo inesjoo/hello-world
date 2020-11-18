e5ad3cb104a39990969d50ba2276c174cf81e9d1
inesjoo <73700665+inesjoo@users.noreply.github.com>
2020-11-18 22:00:06 +0900

    Add files via upload

--- 19강_심사문제.py
+++ 19강_심사문제.py
@@ -1,9 +1,8 @@
 a=int(input())
 for i in range(a):
-    for j in range(a+i):
-        if j<=i:
-            print(' ',end='')
-        elif 
+    for j in range (a+i):
+        if j<=a-2-i:
+            print(' ',sep='',end='')
         else:
-            print('*',end='')
+            print('*',sep='',end='')
     print()
